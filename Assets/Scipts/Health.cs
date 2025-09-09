17fa001fffe8e65c8f4ea173467d1a506d1a396c
Alex <alex.n.portal@gmail.com>
2025-09-09 18:12:33 +0200

    Implement DoDamage logic

--- Assets/Scipts/Health.cs
+++ Assets/Scipts/Health.cs
@@ -5,18 +5,24 @@ public class Health : MonoBehaviour
 {
     public Action OnHPChange = delegate { };
     public Action OnDie = delegate { };
-    
+
     public float HP { get; private set; }
     public int MaxHP { get; private set; }
-    
+
     public void Init(int maxHP)
     {
         HP = maxHP;
         MaxHP = maxHP;
     }
 
-    public void DoDamage(int damage)
+    public void DoDamage(float damage)
     {
-        
+        if (HP <= 0) return;
+
+        HP = Mathf.Max(0, HP - Mathf.Max(0, damage));
+        OnHPChange?.Invoke();
+
+        if (HP <= 0)
+            OnDie?.Invoke();
     }
 }
